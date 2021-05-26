eab5b6a452b1b7cd432ba336ec5a73223e02deb6
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-05-26 17:43:31 +0000

    setup correct - fin

---
+++ app/models/like.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fan_id     :integer
+#  photo_id   :integer
+#  user_id    :integer
+#
+class Like < ApplicationRecord
+
+  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
+  belongs_to(:photo, { :required => false, :class_name => "Photo", :foreign_key => "photo_id", :counter_cache => true })
+
+  validates(:user_id, { :presence => true })
+  validates(:photo_id, { :presence => true })
+  validates(:photo_id, { :uniqueness => { :scope => ["user_id"], :message => "already liked" } })
+end
